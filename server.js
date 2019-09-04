310497f78a95f6a11747970714fe9de99c897926
TK Farewell <mfarewell@ufl.edu>
2019-09-04 19:02:46 -0400

    completed assignment by updating server.js

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// CEN3031 - ASSIGNMENT 1
+// TK FAREWELL
+
 var http = require('http'), 
     fs = require('fs'), 
     url = require('url'),
@@ -6,8 +9,8 @@ var http = require('http'),
 /* Global variables */
 var listingData, server;
 
-var requestHandler = function(request, response) {
-  var parsedUrl = url.parse(request.url);
+
+// ------------------------------------------------------------------------- //
 
   /*
     Your request handler should send listingData in the JSON format as a response if a GET request 
@@ -24,28 +27,52 @@ var requestHandler = function(request, response) {
     HINT: Explore the list of MIME Types
     https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types
    */
+   
+// ------------------------------------------------------------------------- //
+
+var requestHandler = function(request, response) {
+  var parsedUrl = url.parse(request.url);  
+
+	// send listingData in the JSON format as a response if a GET request 
+	// is sent to the '/listings' path
+    if( parsedUrl.pathname == '/listings' ){
+    
+        response.writeHead(200, {'Content-Type': 'text/html'});
+		response.write(listingData);
+    	response.end();
+	}
+	// Otherwise, it should send a 404 error
+    else{
+        response.writeHead(404, {'Content-Type': 'text/html'});
+		response.write("Bad gateway error");
+    	response.end();
+    }
 };
 
-fs.readFile('listings.json', 'utf8', function(err, data) {
+// ------------------------------------------------------------------------- //
+
   /*
     This callback function should save the data in the listingData variable, 
     then start the server. 
 
     HINT: Check out this resource on fs.readFile
-    //https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback
+    //    https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback
 
     HINT: Read up on JSON parsing Node.js
-   */
+   */  
 
+fs.readFile('listings.json', 'utf8', function(err, data) {
     //Check for errors
-  
-
+	if (err) { throw err; }
+	
    //Save the sate in the listingData variable already defined
-  
+	listingData = data;
 
-  //Creates the server
-  
+  //Creates the server  
+	server = http.createServer(requestHandler);
+
   //Start the server
-
-
+	server.listen(port);
+	console.log('Server listening on: http://localhost:' + port);
 });
+
